a2c8de39b258f5cc7f06c9c9ba5c88f09ca25ec8
Rafa <rafaleonbb@gmail.com>
2019-11-05 22:03:28 +0100

    Cambiar la capa Dal para que use EF Core + SQLite

--- Dal/Context.cs
+++ Dal/Context.cs
@@ -1,46 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
 using MultiLayerSolution.Model;
 
 namespace MultiLayerSolution.Dal
 {
-	/// <summary>
-	/// Esta clase implementará la capa de acceso a datos (Entity Framework u otro ORM)
-	/// </summary>
-	public class Context : IDisposable
+	public class Context : DbContext
 	{
-		static List<Customer> customers;
+		public DbSet<Customer> Customers { get; set; }
 
-		/// <summary>
-		/// Acceso a la tabla de clientes
-		/// </summary>
-		public List<Customer> Customers => customers;
-
-		/// <summary>
-		/// Inicializa una nueva clase de <see cref="Context"/>
-		/// </summary>
-		static Context () => Seed ();
-
-		/// <summary>
-		/// Crear algunos registros en las tablas
-		/// </summary>
-		static void Seed () => SeedCustomers ();
-
-		/// <summary>
-		/// Crear algunos registro en la tabla de clientes
-		/// </summary>
-		static void SeedCustomers ()
+		protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
 		{
-			customers = new List<Customer> {
-				new Customer { Id = 1, Name = "Come Jillón", Address = "Paula Montal", Phone = "555 555 555"},
-				new Customer { Id = 2, Name = "Ines Table", Address = "Av Cervantes", Phone = "555 666 777"},
-				new Customer { Id = 3, Name = "Ángela Tina", Address = "Noruega", Phone = "555 888 555"}
-			};
-		}
-
-		public void Dispose ()
-		{
-			// Cerrar base de datos
+			optionsBuilder.UseSqlite ("Data Source=MultiLayerSolution.db;");
 		}
 	}
 }
