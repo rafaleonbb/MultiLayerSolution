a40866468a6e89f1681887b7976e330fb2c1fc6c
Rafa <rafaleonbb@gmail.com>
2019-11-07 21:01:57 +0100

    Comentario

--- Bll/Customers/CustomersLogic.cs
+++ Bll/Customers/CustomersLogic.cs
@@ -5,6 +5,7 @@ namespace MultiLayerSolution.Bll.Customers
 {
 	class CustomersLogic : LogicInfrastructure<Customer>
 	{	
-
+		// Operaciones CRUD estándar implementadas LogicInfrastructure
+		// Implementar aquí lógica adicional específica de Customer
 	}
 }
